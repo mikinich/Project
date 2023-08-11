5bb6e45b4e447d8321b1c0cccc4551bbfcb8572b
mikinich <scam31874@gmail.com>
2023-08-11 17:16:03 +0300

    Django project request lesson 8

--- mysite/advertisements/admin.py
+++ mysite/advertisements/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'created_date', 'auction']
+    list_filter = ['auction', 'created_at']
+    actions = ["mark_auction_as_true", "mark_auction_as_false"]
+    fieldsets = (
+        ('Общее', {
+            'fields': ('title', 'description'),
+            'classes': ['collapse']
+        }),
+        ('Финансы', {
+            'fields': ('price', 'auction'),
+            'classes': ['collapse']
+        }),
+    )
+    search_fields = ("title",)
+
+
+    @admin.action(description="Добавить возможность торга")
+    def mark_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+
+    @admin.action(description="Убрать возможность торга")
+    def mark_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+# admin.site.register(Advertisement, AdvertisementAdmin) # 2
+
--- mysite/advertisements/models.py
+++ mysite/advertisements/models.py
@@ -1,9 +1,28 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
-class Adrertisements(models.Model):
-    title = models.CharField(max_length=80)
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=60)
     description = models.TextField()
     price = models.DecimalField(max_digits=12, decimal_places=2)
+    auction = models.BooleanField(help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    auction = models.BooleanField(help_text="Отметьте, если торг уместен")
+
+    @admin.display
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M")
+            return format_html(
+                "<span style='color: green; font-weight: bold'>Сегодня в {}</span>", created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y - %H:%M")
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, price={self.price})"
+
+    class Meta:
+        db_table = "advertisements"
